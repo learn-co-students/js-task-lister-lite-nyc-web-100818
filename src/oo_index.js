7627f4ff5400a84c644bef9ed34c7f16f2e3e66a
github username <github email address>
2018-11-20 16:50:06 -0500

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,29 @@
-
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+ const taskList = new TaskList();
+
+ const submitForm = document.querySelector('#create-task-form');
+ const todoList = document.querySelector('#list');
+ const inputForm = document.querySelector('#new-task-description');
+
+ submitForm.addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   let tasks = todoList.querySelector('#tasks')
+
+   let taskElement = document.createElement('li')
+
+   let deleteButtonElement = document.createElement('button')
+   deleteButtonElement.innerHTML = 'X'
+
+   deleteButtonElement.addEventListener('click', function() {
+     taskElement.remove();
+   });
+
+
+   taskElement.innerHTML = inputForm.value;
+   tasks.appendChild(taskElement);
+
+   taskElement.appendChild(deleteButtonElement);
+
+ });
 });
