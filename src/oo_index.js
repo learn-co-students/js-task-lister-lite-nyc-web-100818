34a8388e1440718177f7b62729a748929181b836
minheekangg <minheekangg@gmail.com>
2018-11-21 10:39:47 -0500

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,36 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
+  const taskForm = document.querySelector('#create-task-form')
+const taskDiv = document.querySelector('#tasks') // text will appear here
+// debugger
+taskForm.addEventListener('submit', function (event) {
+  event.preventDefault()
+  const userTask = event.target.querySelector('#new-task-description')
+  const newLiTag = document.createElement('li') // will hold the task once submitted
+  // debugger
+  newLiTag.innerText = userTask.value // user input string
+  taskDiv.appendChild(newLiTag)
+   const userPriority = document.getElementById("priority")
+  // console.log(userPriority.value)
+  // debugger
+  newLiTag.style.color = userPriority.value
+  // userPriority.addEventListener('click', function(){
+  //   console.log(newLiTag)
+  //   // debugger
+  //   // newLiTag.appendChild(userPriority)
+  // })
+   const newDelete = document.createElement("button")
+  newDelete.innerText = "delete me"
+  newLiTag.appendChild(newDelete)
+  // NOTE: var btn = document.createElement("BUTTON");
+  // document.body.appendChild(btn)
+   newDelete.addEventListener('click', function(){
+    newLiTag.remove()
+   })
+  event.target.reset()
+ })
+// find the element - inside inner html
+// do .remove()
+
 });
