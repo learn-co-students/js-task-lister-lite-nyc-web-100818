b73b335f07e819eb53e07d765a6690802a615b6e
ihollander <ianholla@gmail.com>
2018-11-28 12:57:46 -0500

    Done.

--- src/taskList.js
+++ src/taskList.js
@@ -1,2 +1,17 @@
 class TaskList {
+  constructor() {
+    this.tasks = []
+  }
+
+  findTask(description) {
+    return this.tasks.find(task => task.description === description)
+  }
+
+  findTaskIndex(description) {
+    return this.tasks.findIndex(task => task.description === description)
+  }
+
+  render() {
+    return this.tasks.map(task => task.render()).join("")
+  }
 }
